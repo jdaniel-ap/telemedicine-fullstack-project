01b7c2f60ead6281f226ecbcf4caf66cfb9a53c8
jdaniel-ap <jdaniell.93@gmail.com>
2021-10-12 16:57:30 -0300

    ignore dist

--- back-end/dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";var _app = require('./app');
-require('dotenv/config');
-
-const PORT = process.env.PORT;
-
-_app.httpServer.listen(PORT, () => console.log(`App running on port: ${PORT}`));
-  
